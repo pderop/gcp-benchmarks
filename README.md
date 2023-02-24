ae8d997927759262d463b121257599454606c639
gcp-benchmarks Benchmarks <pderop@users.noreply.github.com>
2023-02-24 10:39:34 +0000

    Benchmarks trend

---
+++ README.md
@@ -0,0 +1,21 @@
+# Servers shootout board
+## Results
+
+| Application  | TextPlain | Trends |
+| ---  | :---: | :---: |
+| reactornetty-benchmark-1.0.x-SNAPSHOT | [**result**](bench/reactornetty-benchmark-1.0.x-SNAPSHOT/TextPlain/index.html) | [**result**](bench/reactornetty-benchmark-1.0.x-SNAPSHOT/Trends/index.html) |
+
+## Scenario
+
+Each benchmark case starts with no traffic and does the following:
+
+- increase the concurrency by 128 users (1 users = 1 connection) in 10 seconds
+- hold that concurrency level for 20 seconds
+- go to 1), unless the maximum concurrency of 4096 is reached
+
+## Benchmark cases
+- PlainText: server sends "text/plain" response body
+- Echo: clients sends "text/plain" and the server echoes that in the response
+- JsonGet: server sends a JSON payload
+- JsonPost: client sends a JSON payload, server deserializes it and replies with a JSON payload
+- HtmlGet: the server renders an HTML view with a templating engine
---
+++ bench/reactornetty-benchmark-1.0.x-SNAPSHOT/Trends/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1677235153410,
+  "repoUrl": "https://github.com/pderop/gcp-benchmarks",
+  "entries": {
+    "Trends for reactornetty-benchmark-1.0.x-SNAPSHOT": [
+      {
+        "commit": {
+          "author": {
+            "name": "Pierre De Rop",
+            "username": "pderop",
+            "email": "pderop@vmware.com"
+          },
+          "committer": {
+            "name": "Pierre De Rop",
+            "username": "pderop",
+            "email": "pderop@vmware.com"
+          },
+          "id": "e021633a454f36a209b764bb55174f14944ba256",
+          "message": "wip",
+          "timestamp": "2023-02-24T10:32:04Z",
+          "url": "https://github.com/pderop/gcp-benchmarks/commit/e021633a454f36a209b764bb55174f14944ba256"
+        },
+        "date": 1677235152687,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "reactornetty-benchmark-1.0.x-SNAPSHOT-TextPlain",
+            "value": "17481.1",
+            "unit": "mean requests/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
