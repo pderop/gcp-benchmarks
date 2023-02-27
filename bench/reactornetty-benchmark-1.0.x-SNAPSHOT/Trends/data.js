feabdc8e44872c508429981c93bfa64ca4d52e11
gcp-benchmarks Benchmarks <pderop@users.noreply.github.com>
2023-02-27 21:50:33 +0000

    Benchmarks trend

---
+++ bench/reactornetty-benchmark-1.0.x-SNAPSHOT/Trends/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1677534613253,
+  "repoUrl": "https://github.com/pderop/gcp-benchmarks",
+  "entries": {
+    "Trends for reactornetty-benchmark-1.0.x-SNAPSHOT": [
+      {
+        "commit": {
+          "author": {
+            "name": "Pierre De Rop",
+            "username": "pderop",
+            "email": "pderop@vmware.com"
+          },
+          "committer": {
+            "name": "Pierre De Rop",
+            "username": "pderop",
+            "email": "pderop@vmware.com"
+          },
+          "id": "4287126110",
+          "message": "No new commit since last benchmark.",
+          "timestamp": "2023-02-27T21:35:09Z",
+          "url": "https://github.com/pderop/gcp-benchmarks/actions/runs/4287126110"
+        },
+        "date": 1677534612892,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "reactornetty-benchmark-1.0.x-SNAPSHOT-TextPlain",
+            "value": 18143.6,
+            "unit": "mean requests/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
