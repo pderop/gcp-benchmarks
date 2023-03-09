4aa6163360a9357c924fb1e5d9a03be5b8603397
Pierre De Rop <pderop@vmware.com>
2023-03-09 16:27:26 +0100

    wip

--- containers/client/run.sh
+++ containers/client/run.sh
@@ -8,11 +8,11 @@ SIMULATIONS=$3
 INCREMENT=$4
 STEPS=$5
 DURATION=$6
-APP=$7
+APP_NAME=$7
 PROTOCOL=$8
 BENCHDIR=${9:-/opt/bench}
 
-echo "Running simulations: ${SIMULATIONS} for application $APP"
+echo "Running simulations: ${SIMULATIONS} for application $APP_NAME"
 java -version
 mkdir -p $BENCHDIR/results
 
@@ -37,7 +37,7 @@ curl http://$SERVER_HOST:$SERVER_PORT/text \
 for simulation in $(echo ${SIMULATIONS} | tr ";" "\n"); do
   JOPTS="-DHOST=${SERVER_HOST} -DPORT=${SERVER_PORT} -DINCREMENT=${INCREMENT} -DSTEPS=${STEPS} -DDURATION=${DURATION} -DPROTOCOL=${PROTOCOL}"
 
-  name="$APP-$PROTOCOL-$simulation"
+  name="$APP_NAME-$simulation"
   mean=$(java ${JOPTS} -jar gatling-1.0-SNAPSHOT-all.jar "$name" $simulation | grep "mean requests/sec"|awk  '{print $4}')
   unit="mean requests/sec"
   value="$mean"
@@ -51,9 +51,9 @@ for simulation in $(echo ${SIMULATIONS} | tr ";" "\n"); do
 
   simulation_lower=$(echo "$simulation" | tr '[:upper:]' '[:lower:]')
   rm -f test-reports/$simulation_lower*/simulation.log
-  mkdir -p $BENCHDIR/results/bench/$APP-$PROTOCOL/$simulation
+  mkdir -p $BENCHDIR/results/bench/$APP_NAME/$simulation
   find test-reports
-  mv test-reports/$simulation_lower*/* $BENCHDIR/results/bench/$APP-$PROTOCOL/$simulation/
+  mv test-reports/$simulation_lower*/* $BENCHDIR/results/bench/$APP_NAME/$simulation/
 done
 
 cd $BENCHDIR/results/
