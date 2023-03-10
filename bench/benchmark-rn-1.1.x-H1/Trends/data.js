e733b5fd6d6ad53bcd72f2c13cff3b9d05b6ae32
gcp-benchmarks Benchmarks <pderop@users.noreply.github.com>
2023-03-10 21:31:04 +0000

    Benchmarks

--- bench/benchmark-rn-1.1.x-H1/Trends/data.js
+++ bench/benchmark-rn-1.1.x-H1/Trends/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678409210718,
+  "lastUpdate": 1678483631673,
   "repoUrl": "https://github.com/pderop/gcp-benchmarks",
   "entries": {
     "Trends for benchmark-rn-1.1.x-H1": [
@@ -31,6 +31,33 @@ window.BENCHMARK_DATA = {
           {
             "name": "benchmark-rn-1.1.x-H1-Remote",
             "value": 8243.017,
+            "unit": "mean requests/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Pierre De Rop",
+            "username": "pderop",
+            "email": "pderop@vmware.com"
+          },
+          "committer": {
+            "name": "Pierre De Rop",
+            "username": "pderop",
+            "email": "pderop@vmware.com"
+          },
+          "id": "4388556419",
+          "message": "No new commit since last benchmark.",
+          "timestamp": "2023-03-10T00:34:31Z",
+          "url": "https://github.com/pderop/gcp-benchmarks/actions/runs/4388556419"
+        },
+        "date": 1678483631331,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "benchmark-rn-1.1.x-H1-TextPlain",
+            "value": 21706.55,
             "unit": "mean requests/sec"
           }
         ]
